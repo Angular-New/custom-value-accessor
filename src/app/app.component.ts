b1363ca45685576d47a98a8631c97478b52592ed
Verda Muso <gearmobile@gmail.com>
2024-04-22 16:44:39 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cva-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'cva-example';
+}
